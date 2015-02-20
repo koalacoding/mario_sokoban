c266e4ee935e959549413305bcc61f983dfe1d73
koalacoding <koalacoding@hello.world>
2015-02-20 14:21:03 +0100

    Rework map_load function which was really bad

--- map.c
+++ map.c
@@ -6,74 +6,84 @@
 
 #include "status.h"
 
+// this is the max number of column supported, this value will help when parsing
+// the map file to determine line buffer size
 #define MAP_COLUMN_MAX 64
 
 // it is overkill for what the exercise is asking, row and column are not
 // hardcoded (should be 12x12)
 Map* map_load(const char* filename) {
     FILE* fp;
-    unsigned int i = 0;
     Map* map = NULL;
     Map* new_map = NULL;
 
     map = (Map*)malloc(sizeof(Map));
     memset(map, 0, sizeof(Map));
 
-    map->sprite_id_count = 64;
-    map->sprite_id = (int*)malloc(map->sprite_id_count * sizeof(int));
-    memset(map->sprite_id, 0, sizeof(int) * map->sprite_id_count);
+    // create a buffer too small, forcing realloc to be executed for the example
+    // the buffer will hold a table of int representing the id of sprites
+    map->sprite_id_buffer_size = 4*sizeof(SpriteId);
+    map->sprite_id = (SpriteId*)malloc(map->sprite_id_buffer_size);
+    memset(map->sprite_id, 0, map->sprite_id_buffer_size);
 
     fp = fopen(filename, "rb");
     if (fp == NULL) {
-        // if fopen fails, errno is set and perror format a message from errno
-        // see http://www.cplusplus.com/reference/cstdio/perror/
+        // if fopen fails, errno is set, perror format a message according to
+        // errno value, see http://www.cplusplus.com/reference/cstdio/perror/
         perror("Cannot open map %s\n");
         goto end;
     }
 
+    // parse line by line the map file
     while (1) {
-        char* line_offset = NULL;
-        unsigned int current_index = 0;
-        const unsigned int max_line = MAP_COLUMN_MAX*(sizeof(int)+1);
-        char line[max_line];
-        memset(line, 0, max_line);
+        // a line is composed of an int (the sprite id) and a space as separator
+        // ex: 1234 567 ...
+        const unsigned int line_size = MAP_COLUMN_MAX*(sizeof(SpriteId)+1);
+        char* token = NULL;
+        char line[line_size];
 
         // read a line to be parsed
-        if (fgets(line, max_line, fp) == NULL) {
-            break;  // no more line to process
+        memset(line, 0, line_size);
+        if (fgets(line, line_size, fp) == NULL) {
+            break;
         }
 
-        // extract integers and get column count
-        current_index = i;
-        line_offset = strtok(line, " ");
-        while (line_offset) {
-            // make buffer large enough
-            if (i >= map->sprite_id_count) {
-                const unsigned int added_entry_count = 2;
-                debug("realloc: %d\n", (map->sprite_id_count +
-                                        added_entry_count)*sizeof(int));
-                int* new_alloc = realloc(map->sprite_id,
-                    (map->sprite_id_count + added_entry_count)*sizeof(int));
+        // extract integers and get column count with space as separator
+        token = strtok(line, " ");
+        while (token) {
+            // make sure buffer is large enough
+            // TODO: this could be struct/function
+            if (map->sprite_id_count*sizeof(SpriteId) >=
+                    map->sprite_id_buffer_size) {
+                // add only 8 items so there will be more realloc (example)
+                const unsigned int new_buffer_size =
+                        map->sprite_id_buffer_size + 8*sizeof(SpriteId);
+
+                int* new_alloc = realloc(map->sprite_id, new_buffer_size);
                 if (new_alloc == NULL) {
-                    fprintf(stderr, "allocation failed\n");
+                    fprintf(stderr, "allocation failed (%d bytes)\n",
+                            new_buffer_size);
                     goto end;
                 }
+                debug("%s:%d realloc %d bytes (%d cells)\n", __FILE__, __LINE__,
+                      new_buffer_size, new_buffer_size/sizeof(SpriteId));
                 map->sprite_id = new_alloc;
-                map->sprite_id_count += added_entry_count;
+                map->sprite_id_buffer_size = new_buffer_size;
             }
-            sscanf(line_offset, "%d", &map->sprite_id[i++]);
-            line_offset = strtok(NULL, " ");
+            sscanf(token, "%d", &map->sprite_id[map->sprite_id_count++]);
+            token = strtok(NULL, " ");
         }
-        if (map->column == 0) {
-            map->column = i - current_index;
+        map->row++;
+
+        // number of column must be constant
+        if (map->row == 1) {
+            map->column = map->sprite_id_count;
         } else {
-            // number of column must always be the same
-            if (map->column != i - current_index) {
-                fprintf(stderr, "invalid map file format");
+            if (map->column * map->row != map->sprite_id_count) {
+                fprintf(stderr, "wrong number of columns, line %d\n", map->row);
                 goto end;
             }
         }
-        map->row++;
     }
     new_map = map;
 end:
--- map.h
+++ map.h
@@ -1,13 +1,18 @@
 #ifndef MAP_H_
 #define MAP_H_
 
+#include "status.h"
+
 enum {
     BLANK, WALL, BOX, OBJECTIVE, MARIO, BOX_OK, SPRITE_COUNT
 } SPRITE;
 
+typedef unsigned int SpriteId;
+
 typedef struct {
-    unsigned int* sprite_id;
-    unsigned int sprite_id_count;  // available sprite entries in the  buffer
+    SpriteId* sprite_id;
+    unsigned int sprite_id_buffer_size;
+    unsigned int sprite_id_count;  // number of loaded sprites
     unsigned int row;
     unsigned int column;
 } Map;
