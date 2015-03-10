a7665cc0eb8ef2f79678f8a2572a27a31d0815b5
koalacoding <koalacoding@hello.world>
2015-03-10 22:28:22 +0100

    Split map_view into Window

---
+++ window.h
@@ -0,0 +1,15 @@
+#ifndef WINDOW_H_
+#define WINDOW_H_
+
+#include <SDL/SDL.h>
+
+typedef struct Window {
+    SDL_Surface* surface;
+} Window;
+
+Window* window_create(const char* caption, const unsigned int width,
+                      const unsigned int height);
+
+void window_destroy(Window* window);
+
+#endif  // WINDOW_H_
